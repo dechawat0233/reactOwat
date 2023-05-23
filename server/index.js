ec5ce6b75a0eaf27438f0565e185aec833ae07f0
WasanPangpounju <friendlydev.net@gmail.com>
2023-05-23 17:23:41 +0700

    update connect mongodb

--- server/index.js
+++ server/index.js
@@ -41,7 +41,6 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 // Set up JWT middleware
 const jwtMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -67,28 +66,53 @@ const userSchema = new mongoose.Schema({
 
 // Define user model
 const User = mongoose.model('User', userSchema);
-
 // Authenticate user and return JWT token
 app.post('/api/auth/login', async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = await req.body;
+console.log(username  + ' ' + password );
 
   // Check if user exists
   const user = await User.findOne({ username });
   if (!user) {
+    console.log('no user');
     return res.status(404).json({ error: 'User not found' });
   }
 
   // Check if password is correct
+  // const passwordMatches = await bcrypt.compare(password, user.password);
   const passwordMatches = await bcrypt.compare(password, user.password);
+
   if (!passwordMatches) {
+    console.log('password can not Matches');
     return res.status(401).json({ error: 'Invalid password' });
   }
 
   // Generate and sign JWT token
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  // const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-  res.json({ token });
+  // Generate and sign JWT token
+  try {
+    const userId = await user._id; // Replace with the actual user ID
+    // const secretKey = process.env.JWT_SECRET; // Replace with your own secret key
+    const secretKey = await 'Friendlydev'; // Replace with your own secret key
+    const expiresIn = await '1h'; // Set the token expiration time
+  console.log(secretKey );
+    if (!secretKey) {
+      throw new Error('Missing JWT secret key');
+    }
+  
+    const token = await jwt.sign({ userId }, secretKey, { expiresIn });
+    await res.json({ token });
+    
+    await console.log('Token:', token);
+  } catch (error) {
+    console.error('Error generating JWT token:', error.message);
+  }
+  
+  
+  // res.json({ token });
 });
+
 
 // Get list of users
 // app.get('/api/users', jwtMiddleware, async (req, res) => {
