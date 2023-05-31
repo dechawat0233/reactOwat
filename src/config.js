6a1cb78ce32aac5f7b82c322535d292815433842
WasanPangpounju <friendlydev.net@gmail.com>
2023-06-01 02:57:10 +0700

    add  /src/config.js for config using in local or server

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 // config.js
 //config for local
 /*
-  loginEndpoint: 'http://localhost:3000/users/auth/login';
+  const loginEndpoint = 'http://localhost:3000/users/auth/login';
 */
 
 //config for server
-  loginEndpoint: 'http://192.168.1.189:3000/users/auth/login'
+  const loginEndpoint = 'http://192.168.1.189:3000/users/auth/login'
 
 export default loginEndpoint;
