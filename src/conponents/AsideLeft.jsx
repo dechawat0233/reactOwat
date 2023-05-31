8c856ccfe438ae5e673a606e0fa0c57b008c1427
WasanPangpounju <friendlydev.net@gmail.com>
2023-06-01 03:28:21 +0700

    get user data

--- src/conponents/AsideLeft.jsx
+++ src/conponents/AsideLeft.jsx
@@ -37,7 +37,7 @@ const token = localStorage.getItem('token');
             </div>
             <div class="info">
               <a href="#" class="d-block">
-                <p>คุณสมศรี รักสะอาด</p><i class="nav-icon fas fa-solid fa-user" style={{ fontSsize: "11px", paddingRight: "5px" }}></i> Super Admin {user}
+                <p>คุณสมศรี รักสะอาด</p><i class="nav-icon fas fa-solid fa-user" style={{ fontSsize: "11px", paddingRight: "5px" }}></i> Super Admin 
               </a>
             </div>
           </div>
