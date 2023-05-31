5c5bb5d7093f21f0e8ec6fd3667595cd57e15503
WasanPangpounju <friendlydev.net@gmail.com>
2023-06-01 03:50:37 +0700

    get user data

--- src/conponents/Login.jsx
+++ src/conponents/Login.jsx
@@ -31,7 +31,6 @@ function LoginForm({ onLogin }) {
                 const { token , user} = response.data;
 
                 localStorage.setItem('token', token);
-      //localStorage.setItem('user', JSON.stringify(user));
       localStorage.setItem('user', user.username;
 
                 localStorage.setItem('loggedIn', 'true');
