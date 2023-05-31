6284b1bdadee19ee01f3aaa19ef4d8b82b7b6863
WasanPangpounju <friendlydev.net@gmail.com>
2023-05-31 11:31:52 +0700

    create employee

--- owatmaid-api/config.js
+++ owatmaid-api/config.js
@@ -1,6 +1,8 @@
-//const url = 'mongodb://localhost:27017/users';
-const url = 'mongodb://172.17.0.2:27017/users';
+//config for local and test
+const connectionString = 'mongodb://localhost:27017/users';
 
+//config for server 
+/*
 const username = 'admin';
 const password = 'friendlydev214';
 const host = '172.17.0.2';
@@ -9,6 +11,6 @@ const port = '27017';
 const database = 'admin';
 
 const connectionString = `mongodb://${username}:${password}@${host}:${port}/${database}`;
-
+*/
 
 module.exports = connectionString;
