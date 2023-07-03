89daf6febdbefd8bda00206c4e3053713b5d4928
dechawat <69790549+dechawat0233@users.noreply.github.com>
2023-07-03 16:19:17 +0700

    save

--- src/conponents/componentsAside/Salary.jsx
+++ src/conponents/componentsAside/Salary.jsx
@@ -26,24 +26,24 @@ function Salary() {
     const [worktimerecord, setWorktimerecord] = useState(''); //ผู้บันทึกเวลา
     const [workrecord, setWorkrecord] = useState(''); //ผู้บันทึกข้อมูลแทน
 
-        const [jobtype, setJobtype] = useState(''); //ประเภทการจ้าง
-        const [startjob, setStartjob] = useState(''); //วันที่เริ่มงาน
-        const [exceptjob, setExceptjob] = useState(''); //วันที่บรรจุ
-        const [startcount , setStartcount] = useState(''); //วันที่บรรจุ
-        const [salary , setSalary] = useState(''); //อัตราเงินเดือน
-        const [salarytype , setSalarytype] = useState(''); //อัตราเงินเดือน
-        const [salaryupdate , setSalaryupdate] = useState(''); //เงินเดือนปรับเมื่อ
-        const [salaryout , setSalaryout] = useState(''); //เงินเดือนปรับเมื่อ
-        const [salarypayment, setSalarypayment] = useState(''); //วิธีจ่ายเงิน
-        const [salarybank , setSalarybank] = useState(''); //ธนาคาร
-        const [banknumber , setBanknumber] = useState(''); //เลขบัญชี
-        const [salaryadd1 , setSalaryadd1] = useState(''); //เงินเพิ่มพิเศษ ค่ารถ
-        const [salaryadd2 , setSalaryadd2] = useState(''); //เงินเพิ่มพิเศษ ค่าอาหาร
+    const [jobtype, setJobtype] = useState(''); //ประเภทการจ้าง
+    const [startjob, setStartjob] = useState(''); //วันที่เริ่มงาน
+    const [exceptjob, setExceptjob] = useState(''); //วันที่บรรจุ
+    const [startcount, setStartcount] = useState(''); //วันที่บรรจุ
+    const [salary, setSalary] = useState(''); //อัตราเงินเดือน
+    const [salarytype, setSalarytype] = useState(''); //อัตราเงินเดือน
+    const [salaryupdate, setSalaryupdate] = useState(''); //เงินเดือนปรับเมื่อ
+    const [salaryout, setSalaryout] = useState(''); //เงินเดือนปรับเมื่อ
+    const [salarypayment, setSalarypayment] = useState(''); //วิธีจ่ายเงิน
+    const [salarybank, setSalarybank] = useState(''); //ธนาคาร
+    const [banknumber, setBanknumber] = useState(''); //เลขบัญชี
+    const [salaryadd1, setSalaryadd1] = useState(''); //เงินเพิ่มพิเศษ ค่ารถ
+    const [salaryadd2, setSalaryadd2] = useState(''); //เงินเพิ่มพิเศษ ค่าอาหาร
 
-        const [salaryaddtype , setSalaryaddtype] = useState(''); //เพิ่มพิเศษแบบ ต่อวัน ต่อเดือน
-        const [salaryaddsum , setSalaryaddsum] = useState(''); //เพิ่มพิเศษแบบ ต่อวัน ต่อเดือน
-        const [salaryadd1v , setSalaryadd1v] = useState(''); //จำนวนเงินเพิ่มพิเศษ ค่ารถ 
-        const [salaryadd2v , setSalaryadd2v] = useState(''); //จำนวนเงินเพิ่มพิเศษ ค่าอาหาร
+    const [salaryaddtype, setSalaryaddtype] = useState(''); //เพิ่มพิเศษแบบ ต่อวัน ต่อเดือน
+    const [salaryaddsum, setSalaryaddsum] = useState(''); //เพิ่มพิเศษแบบ ต่อวัน ต่อเดือน
+    const [salaryadd1v, setSalaryadd1v] = useState(''); //จำนวนเงินเพิ่มพิเศษ ค่ารถ 
+    const [salaryadd2v, setSalaryadd2v] = useState(''); //จำนวนเงินเพิ่มพิเศษ ค่าอาหาร
 
 
     const [prefix, setPrefix] = useState(''); //นำหน้าชื่อ
@@ -59,23 +59,23 @@ function Salary() {
         setWorkplace(event.target.value);
     };
 
-        const handleSalaryadd1= (event) => {
-if(salaryadd1 !== ''){
-    setSalaryadd1('');
-}else {
-    setSalaryadd1(event.target.value);
-}
-        };
-        const handleSalaryadd2 = (event) => {
-            if(salaryadd2 !== ''){
-                setSalaryadd2('');
-            } else {
-                setSalaryadd2(event.target.value);
-            }
-        };
-        const handleSalaryaddtype= (event) => {
-            setSalaryaddtype(event.target.value);
-        };
+    const handleSalaryadd1 = (event) => {
+        if (salaryadd1 !== '') {
+            setSalaryadd1('');
+        } else {
+            setSalaryadd1(event.target.value);
+        }
+    };
+    const handleSalaryadd2 = (event) => {
+        if (salaryadd2 !== '') {
+            setSalaryadd2('');
+        } else {
+            setSalaryadd2(event.target.value);
+        }
+    };
+    const handleSalaryaddtype = (event) => {
+        setSalaryaddtype(event.target.value);
+    };
 
 
     return (
@@ -560,23 +560,23 @@ if(salaryadd1 !== ''){
                                                         </div>
                                                     </div>
 
-{salaryadd1 && (
+                                                    {salaryadd1 && (
                                                         <div class="col-md-4">
-                                                        <div class="form-group">
-                                                            <label role="salaryadd1v">เงินเพิ่มค่ารถ</label>
-                                                            <input type="text" class="form-control" id="salaryadd1v" placeholder="ค่ารถ" value={salaryadd1v} onChange={(e) => setSalaryadd1v(e.target.value)} />
-</div>
-</div>
-)}
+                                                            <div class="form-group">
+                                                                <label role="salaryadd1v">เงินเพิ่มค่ารถ</label>
+                                                                <input type="text" class="form-control" id="salaryadd1v" placeholder="ค่ารถ" value={salaryadd1v} onChange={(e) => setSalaryadd1v(e.target.value)} />
+                                                            </div>
+                                                        </div>
+                                                    )}
 
-{salaryadd2 && (
+                                                    {salaryadd2 && (
                                                         <div class="col-md-4">
-                                                        <div class="form-group">
-                                                            <label role="salaryadd2v">เงินเพิ่มค่าอาหาร</label>
-                                                            <input type="text" class="form-control" id="salaryadd2v" placeholder="ค่าอาหาร" value={salaryadd2v} onChange={(e) => setSalaryadd2v(e.target.value)} />
-</div>
-</div>
-)}
+                                                            <div class="form-group">
+                                                                <label role="salaryadd2v">เงินเพิ่มค่าอาหาร</label>
+                                                                <input type="text" class="form-control" id="salaryadd2v" placeholder="ค่าอาหาร" value={salaryadd2v} onChange={(e) => setSalaryadd2v(e.target.value)} />
+                                                            </div>
+                                                        </div>
+                                                    )}
 
                                                     <div class="col-md-4">
                                                         <div class="form-group">
