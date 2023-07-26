ad101d9fa0dbb864e504264b3ea06379ddcc74cb
dechawat <69790549+dechawat0233@users.noreply.github.com>
2023-07-26 11:16:00 +0700

    save

--- src/conponents/componentsAside/TestPDF.jsx
+++ src/conponents/componentsAside/TestPDF.jsx
@@ -1,71 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
 
 const TestPDF = () => {
-    const generatePDF = () => {
-        const doc = new jsPDF('landscape');
+  const [data, setData] = useState([]);
 
-        // Load the Thai font
-        const fontPath = '/assets/fonts/THSarabunNew.ttf';
-        doc.addFileToVFS(fontPath);
-        doc.addFont(fontPath, 'THSarabunNew', 'normal');
+  const generatePDF = async () => {
+    try {
+      const response = await fetch('http://68.183.230.164:3000/users/list');
+      const jsonData = await response.json();
+      setData(jsonData);
+      console.log(jsonData); // Check if the data is received and stored correctly
 
-        // Override the default styles for jspdf-autotable
-        const styles = {
-            font: 'THSarabunNew',
-            fontStyle: 'normal',
-            fontSize: 20,
-        };
-        const tableOptions = {
-            styles: styles,
-            startY: 20,
-            margin: { top: 10 },
-        };
+      const doc = new jsPDF('landscape');
 
-        const title = 'Sample PDF Title';
-        const data = [
-            ['Name', 'Age', 'City'],
-            ['John Doe', '30', 'New York'],
-            ['Jane Smith', '25', 'Los Angeles'],
-            ['Bob Johnson', '40', 'Chicago'],
-            ['ทดสอบ ว่า', 'ทำงานได้ไหม', 'ฮุฮ่าๆๆ'],
-        ];
+      // Load the Thai font
+      const fontPath = '/assets/fonts/THSarabunNew.ttf';
+      doc.addFileToVFS(fontPath);
+      doc.addFont(fontPath, 'THSarabunNew', 'normal');
 
-        // Set title with the Thai font
-        doc.setFont('THSarabunNew');
-        doc.setFontSize(16);
-        const titleWidth = doc.getStringUnitWidth(title) * doc.internal.getFontSize() / doc.internal.scaleFactor;
-        const pageWidth = doc.internal.pageSize.getWidth();
-        const titleX = (pageWidth - titleWidth) / 2;
-        doc.text(title, titleX, 10);
+      // Override the default styles for jspdf-autotable
+      const styles = {
+        font: 'THSarabunNew',
+        fontStyle: 'normal',
+        fontSize: 20,
+      };
+      const tableOptions = {
+        styles: styles,
+        startY: 20,
+        margin: { top: 10 },
+      };
 
-        doc.text('ฮ่าโหลๆ ได้ไหม', 10, 10);
+      const title = 'Sample PDF Title';
 
-        // Create header and data arrays
-        const header = data[0];
-        const tableData = data.slice(1);
+      // Set title with the Thai font
+      doc.setFont('THSarabunNew');
+      doc.setFontSize(16);
+      const titleWidth = doc.getStringUnitWidth(title) * doc.internal.getFontSize() / doc.internal.scaleFactor;
+      const pageWidth = doc.internal.pageSize.getWidth();
+      const titleX = (pageWidth - titleWidth) / 2;
+      doc.text(title, titleX, 10);
 
-        // Add header and data to the table
-        doc.autoTable({
-            head: [header],
-            body: tableData,
-            columnStyles: {
-                0: { cellWidth: 40 },
-                1: { cellWidth: 20 },
-                2: { cellWidth: 40 },
-            },
-            ...tableOptions,
-        });
+      doc.text('ฮ่าโหลๆ ได้ไหม', 10, 10);
 
-        doc.save('example.pdf');
-    };
+      // Create header and data arrays
+      const header = ['Name', 'Email', 'Role'];
+      const tableData = data.map(({ name, email, role }) => [name, email, role]); // Extract only name, email, and role from the data
+      console.log(header);
+      console.log(tableData); // Check if the header and tableData are formed correctly
 
-    return (
-        <div>
-            <button onClick={generatePDF}>Generate PDF</button>
-        </div>
-    );
+      // Add header and data to the table
+      doc.autoTable({
+        head: [header],
+        body: tableData,
+        columnStyles: {
+          0: { cellWidth: 40 },
+          1: { cellWidth: 70 },
+          2: { cellWidth: 40 },
+        },
+        ...tableOptions,
+      });
+
+      doc.save('example.pdf');
+    } catch (error) {
+      console.error('Error generating PDF:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={generatePDF}>Generate PDF</button>
+    </div>
+  );
 };
 
 export default TestPDF;
