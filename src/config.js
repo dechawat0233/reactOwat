0d7a0cfc069abe5e806170782c76b25dfa991315
WasanPangpounju <friendlydev.net@gmail.com>
2023-08-20 11:02:03 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // config.js
 //config for local
-//   const endpoint = 'http://localhost:3000';
+   const endpoint = 'http://localhost:3000';
 
 //config for server test
 //const endpoint = 'http://68.183.230.164:3000';
@@ -8,7 +8,7 @@
 //config for server
 
 //  const endpoint = 'http://192.168.1.189:3000'
-   const endpoint = 'http://192.168.1.189:3000'
+//   const endpoint = 'http://192.168.1.189:3000'
 
 
 export default endpoint;
