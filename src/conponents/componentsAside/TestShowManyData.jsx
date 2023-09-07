8d53ab0bbf0e2f7127f46b30797f820ff9b80b35
dechawat <69790549+dechawat0233@users.noreply.github.com>
2023-09-07 10:40:00 +0700

    save

--- src/conponents/componentsAside/TestShowManyData.jsx
+++ src/conponents/componentsAside/TestShowManyData.jsx
@@ -1,78 +1,93 @@
-import React, { useEffect, useState } from 'react';
-import { useTable } from 'react-table'; // Add this import
-
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-
-import EmployeesSelected from './EmployeesSelected';
+import React from 'react';
 import TestJson from './Testjson.json';
+import { useTable } from 'react-table';
+
 
 function TestShowManyData() {
-  console.log(TestJson);
+  // Filter the data for the desired workplaceId (e.g., "9999")
+  const filteredData = TestJson.filter((item) => item.workplaceId === '9999');
 
-    // Define the columns for your table
-    const columns = React.useMemo(
-        () => [
-            {
-                Header: 'Workplace Name',
-                accessor: 'workplaceName',
-            },
-            {
-                Header: 'Date',
-                accessor: 'date',
-            },
-            {
-                Header: 'Shift',
-                accessor: 'employeeRecord[0].shift', // Assuming you want the shift from the first employeeRecord item
-            },
-            {
-                Header: 'OT Time',
-                accessor: 'employeeRecord[0].otTime', // Assuming you want the OT time from the first employeeRecord item
-            },
-        ],
-        []
-    );
+  // Create an array to hold the data for the table
+  const tableData = [];
 
-    // Create a table instance
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = useTable({
-        columns,
-        data: TestJson, // Pass the JSON data to the table
+  // Extract employee records from the filtered data
+  filteredData.forEach((item) => {
+    item.employeeRecord.forEach((record) => {
+      const staffId = record.staffId;
+      const staffName = record.staffName;
+      const shift = record.shift;
+
+      // Create a new object representing a row in the table
+      const rowData = {
+        staffId,
+        staffName,
+        shift,
+      };
+
+      // Push the row data into the tableData array
+      tableData.push(rowData);
     });
+  });
 
-    return (
-        <div>
-            <h1>Table from JSON Data</h1>
-            <table {...getTableProps()} className="table">
-                <thead>
-                    {headerGroups.map((headerGroup) => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                        prepareRow(row);
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map((cell) => {
-                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-                                })}
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
+  // Define the columns for your table
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'staffId',
+        accessor: 'staffId',
+      },
+      {
+        Header: 'staffName',
+        accessor: 'staffName',
+      },
+      {
+        Header: 'shift',
+        accessor: 'shift',
+      },
+    ],
+    []
+  );
+
+  // Create a table instance
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data: tableData, // Use the transformed tableData
+  });
+
+  return (
+    <div>
+      <h1>Table from JSON Data</h1>
+      <table {...getTableProps()} className="table">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default TestShowManyData
+export default TestShowManyData;
