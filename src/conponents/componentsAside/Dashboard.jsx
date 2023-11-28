2b022bc96ba038e581cb3b2d9eec0f53d596f7c2
dechawat <69790549+dechawat0233@users.noreply.github.com>
2023-11-28 10:00:09 +0700

    save

--- src/conponents/componentsAside/Dashboard.jsx
+++ src/conponents/componentsAside/Dashboard.jsx
@@ -39,7 +39,8 @@ function Dashboard() {
             <div class="container-fluid">
               <div class="Inner-dash">
                 <div class="row">
-                  <a href="" class="btn-d btn-app" style={{ background: "#ff6c60" }}><i class="fas fa-business-time"></i> ระบบลงเวลา</a>
+                  {/* <a href="" class="btn-d btn-app" style={{ background: "#ff6c60" }}><i class="fas fa-business-time"></i> ระบบลงเวลา</a> */}
+                  <Link to="/addsettime" className="btn-d btn-app" style={{ background: "#ff6c60"  }}><i class="fas fa-business-time"></i> ระบบลงเวลา</Link>
                   <Link to="/addEdit_Employee" className="btn-d btn-app" style={{ background: "#9DBAEA" }}><i class="fas fa-people-arrows"></i> ระบบ เพิ่ม/ลบ พนักงาน</Link>
                   <Link to="/addEdit_SalaryEmployee" className="btn-d btn-app" style={{ background: "#A2B4D2" }}><i class="fas fa-money-bill-wave"></i> ระบบ เงินเพิ่ม/เงินหัก พนักงาน</Link>
                   {/* <a href="" class="btn-d btn-app" style={{ background: "#9DBAEA" }}></a>
