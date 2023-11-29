e1f1ee467d2158a3982bd95a3526016c8b76ff26
dechawat <69790549+dechawat0233@users.noreply.github.com>
2023-11-29 15:34:54 +0700

    edit

--- src/conponents/componentsAside/SendEmployeePDF2.jsx
+++ src/conponents/componentsAside/SendEmployeePDF2.jsx
@@ -534,7 +534,7 @@ const SendEmployeePDF2 = () => {
             doc.text(`${value.Id}`, x2, y + (y2 * 2));
             doc.text(`${value.Name}`, x2, y + (y2 * 3));
             doc.text(`${value.age}`, x2, y + (y2 * 4));
-            // doc.text(`${formatDateThai(value.dateOfBirth)}`, x2, y + (y2 * 5));
+            doc.text(`${formatDateThai(value.dateOfBirth)}`, x2, y + (y2 * 5));
             doc.text(`${value.idCard}`, x2, y + (y2 * 6));
             // doc.text(`${value.address}`, x2, y + 70);
             // doc.text(`${value.currentAddress}`, x2, y + (y2 * (7 + textLines.length)));
