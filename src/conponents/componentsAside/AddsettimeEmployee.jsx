8819cc09f50683d9139ae07f77b425074473630a
WasanPangpounju <friendlydev.net@gmail.com>
2024-01-24 15:15:54 +0700

    update 24-01-2024

--- src/conponents/componentsAside/AddsettimeEmployee.jsx
+++ src/conponents/componentsAside/AddsettimeEmployee.jsx
@@ -66,6 +66,7 @@ const [checkaddData , setCheckaddData] = useState('');
     //Workplace data
     const [employeeId, setEmployeeId] = useState(''); //รหัสหน่วยงาน
     const [name, setName] = useState(''); //ชื่อหน่วยงาน
+    const [lastName, setLastname] = useState(''); //ชื่อหน่วยงาน
     const [workplacestay, setWorkplacestay] = useState(''); //สังกัด
     const [workplaceArea, setWorkplaceArea] = useState(''); //สถานที่ปฏิบัติงาน
     const [workOfWeek, setWorkOfWeek] = useState(''); //วันทำงานต่อสัปดาห์
