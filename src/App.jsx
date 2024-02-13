7ede85d1333b5769323f204a7d976acf634e7b89
WasanPangpounju <friendlydev.net@gmail.com>
2024-02-13 11:59:39 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -47,8 +47,8 @@ import Addsettime from "./conponents/componentsAside/Addsettime";
 
 import Salarysummary from "./conponents/componentsAside/Salarysummary";
 // import Examine from "./conponents/componentsAside/salarysummary/examine";
-import Salaryresult from "./conponents/componentsAside/salarysummary/salaryresult";
-import Compensation from "./conponents/componentsAside/salarysummary/compensation";
+// import Salaryresult from "./conponents/componentsAside/salarysummary/salaryresult";
+// import Compensation from "./conponents/componentsAside/salarysummary/compensation";
 
 import Worktimesheet from "./conponents/componentsAside/Worktimesheet";
 import WorktimeSheetWorkplace from "./conponents/componentsAside/WorktimeSheetWorkplace";
