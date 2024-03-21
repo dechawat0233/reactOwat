2244237600a4d0f342f60c7bcab75eee0eb824b0
WasanPangpounju <friendlydev.net@gmail.com>
2024-03-21 13:30:55 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -56,6 +56,7 @@ router.get('/listdelete', async (req, res) => {
 
   try {
     const concludeData = await conclude.find();
+    concludeData.deleteMany();
     res.json(concludeData );
   
     // const concludeData = await Conclude.find();
