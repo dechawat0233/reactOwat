dc6587371d9eafc337216bfbd28bfa3cdbf634ba
WasanPangpounju <friendlydev.net@gmail.com>
2024-05-29 11:50:43 +0700

    update

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -3867,7 +3867,7 @@ let sumAddSalaryAfterTaxTmp = 0;
 //check addSalary with cal tax and social 
 await (async () => {
   await Promise.all(addSalaryList.map(async item => {
-    total  = await total  + parseFloat(item.SpSalary || 0);
+
     if(item.id === '1230' || item.id === '1350' || item.id === '1520' || item.id === '1535' || item.id === '1410') {
       if(item.id === '1230') {
           data.accountingRecord.amountPosition = await item.SpSalary || 0;
@@ -3917,7 +3917,8 @@ if(item.id === '1410') {
       sumAddSalaryAfterTaxTmp += parseFloat(item.SpSalary);
     }
   }
-  
+      total  = await total  + parseFloat(item.SpSalary || 0);
+
   }));
 
   sumAddSalaryBeforeTax = sumAddSalaryBeforeTaxTmp;
