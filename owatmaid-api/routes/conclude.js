a456570e232a3e52e427c5409a8c4b1607e232ff
WasanPangpounju <friendlydev.net@gmail.com>
2024-06-26 03:15:07 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -292,7 +292,7 @@ return dateA - dateB;
   if(data.recordworkplace.length !== 0) {
 //get workplaceId in first employee_workplaceRecord
 // let wpId = data.recordworkplace[0].employee_workplaceRecord[0].workplaceId;
-let wpId  = dataEmp.employees[0].workPlace;
+let wpId  = dataEmp.employees[0].workplace || '';
 const wpDataCalculator = {
   month: month || '',
   year: year || '',
