bb93d430317e6ccbfaa500991d8392a87fdba3e3
WasanPangpounju <friendlydev.net@gmail.com>
2024-07-09 06:13:59 +0700

    update

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -1127,7 +1127,7 @@ router.post('/calsalarylist', async (req, res) => {
     
           for (let c = 0; c < responseConclude.data.recordConclude.length; c++) {
     //check accounting record in database
-    let empId = await responseConclude.data.recordConclude[c].employeeId;
+    let empId = await responseConclude.data.recordConclude[c].employeeId || '';
 
           // Log the values to debug
           console.log(`Searching for year: ${year}, month: ${month}, empId: ${empId}`);
