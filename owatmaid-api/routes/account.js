30f6d8e07e53eeb71d862bb01b8dcabab1dfdb21
WasanPangpounju <friendlydev.net@gmail.com>
2024-07-24 11:19:00 +0700

    update

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -862,7 +862,7 @@ if (data?.accountingRecord?.amountHardWorking ?? false) {
 
 // await console.log(sumSocial );
 
-const intersection = await workDaylist.filter(day => specialDaylist.includes(day));
+const intersection = await workDaylist.filter(day => specialDaylist.includes(day+ ''));
 console.log('workDaylist :' + JSON.stringify(workDaylist, null,2) );
 console.log('');
 console.log('specialDaylist ' + JSON.stringify(specialDaylist,null,2) );
