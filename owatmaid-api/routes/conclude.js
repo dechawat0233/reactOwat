eba782b5cef362577ec4d16ad8165e46f4b2ef65
WasanPangpounju <friendlydev.net@gmail.com>
2024-07-31 12:22:29 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -362,8 +362,10 @@ tmp.shift = element.shift || 0;
 
 //check employee working in multi workplace
     const wGroup = await groupByWorkplaceId(data.recordworkplace[0].employee_workplaceRecord);
-// await console.log('wGroup  :' + JSON.stringify(wGroup,2,null));
-await console.log('wGroup  :' + wGroup.length);
+await console.log('wGroup  :' + JSON.stringify(wGroup,2,null));
+for (const element of wGroup ) {
+console.log('element : ' + element );
+}
 
 //get workplaceId in first employee_workplaceRecord
 // let wpId = data.recordworkplace[0].employee_workplaceRecord[0].workplaceId;
