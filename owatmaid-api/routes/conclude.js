b27db5b9e11785e377d8cbab9c9b684db04321ee
WasanPangpounju <friendlydev.net@gmail.com>
2024-07-31 16:41:18 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -395,7 +395,7 @@ for (const element of data.recordworkplace[0].employee_workplaceRecord) {
   const tmp = {};
 
 const tmpWP = wCalList.find(item => item.workplaceId === element.workplaceId);
-  console.log('workplaceId : ' + tmpWP.workRate );
+  console.log('workplaceId : ' + JSON.stringify(tmpWP ,2 null) );
 
 } //end for
 }
