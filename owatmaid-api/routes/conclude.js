c1475a0cba2cc3943726544c5208913ae1d7d501
WasanPangpounju <friendlydev.net@gmail.com>
2024-08-01 15:45:44 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -373,6 +373,7 @@ let wpId  = dataEmp.employees[0].workplace || '';
 let salary = dataEmp.employees[0].salary || 0;
 
 if(wGroup    ){
+  console.log('wGroup    ' + JSON.stringify(wGroup    ,2,null))
 if(Object.keys(wGroup).length > 1) {
   for (const workplaceId of Object.keys(wGroup)) {
     const group = wGroup[workplaceId];
