9879f44ae2fd81696adf91d31e37a52d124a04e1
WasanPangpounju <friendlydev.net@gmail.com>
2024-08-05 16:21:03 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -152,7 +152,8 @@ router.post('/autocreate', async (req, res) => {
 
     // await console.log('*x ' + JSON.stringify(data1.recordworkplace , null ,2) );
     if (data1.recordworkplace.length !== 0) {
-      data1 = await sortByDate(data1);
+      const sortedData1 = await sortByDate(data1);      
+      data1 = await sortedData1 ;
       //get workplaceId in first employee_workplaceRecord
       // let wpId1 = await data1.recordworkplace[0].employee_workplaceRecord[0].workplaceId;
       let wpId1 = dataEmp.employees[0].workplace || '';
@@ -1456,9 +1457,13 @@ function groupByWorkplaceId(records) {
 const sortByDate = (data) => {
   data.forEach(employee => {
     employee.employee_workplaceRecord.sort((a, b) => {
-      const dateA = a.date.split('/').reverse().join('-');
-      const dateB = b.date.split('/').reverse().join('-');
-      return new Date(dateA) - new Date(dateB);
+      const [dayA, monthA, yearA] = a.date.split('/');
+      const [dayB, monthB, yearB] = b.date.split('/');
+      
+      const dateA = new Date(`${yearA}-${monthA}-${dayA}`);
+      const dateB = new Date(`${yearB}-${monthB}-${dayB}`);
+      
+      return dateA - dateB;
     });
   });
   return data;
