086abfc77db8c34451665b2dbea7c9327551c368
WasanPangpounju <friendlydev.net@gmail.com>
2024-08-13 23:41:57 +0700

    update

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -112,7 +112,7 @@ if(accountData ) {
   await console.log('* isset accounting');
   // await console.log(accountData );
 await dataList .push(accountData );
-    // await res.json(dataList );
+    await res.json(dataList );
 
 } else {
   await console.log('* accounting not save');
