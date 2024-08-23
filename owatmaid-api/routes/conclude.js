c5228bb13e0bc1cda96360c3da5c2fd6d67431ea
WasanPangpounju <friendlydev.net@gmail.com>
2024-08-23 14:34:28 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -1181,10 +1181,10 @@ console.log('tmpWP.data.workRate ' + tmpWP.data.workRate + 'salary '+ salary);
     // await console.log('add salary = ' + JSON.stringify( responseWpList .data ));
     // await console.log('add salary = ' + responseWpList .data.ans.length );
 
-    for (let c = 0; c < 10; c++) {
+    for (let c = 0; c < concludeRecord.length; c++) {
       // console.log('concludeRecord ' + concludeRecord [c].workplaceId);
-console.log('multiply' + parseFloat(concludeRecord [c].workRateMultiply));
-      if(parseFloat(concludeRecord [c].workRateMultiply) == 1) {
+
+      if(parseFloat(concludeRecord [c].workRateMultiply) <= 1) {
       if(responseWpList .data.ans && concludeRecord [c].workplaceId !== '399-105') {
       const testx = responseWpList .data.ans.find(item  => item.workplaceId == concludeRecord [c].workplaceId)
 if(testx ) {
