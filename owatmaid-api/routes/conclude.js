08dd5daf1159e05f903037c77e1a9741adbc87ab
WasanPangpounju <friendlydev.net@gmail.com>
2024-08-26 17:00:28 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -469,8 +469,7 @@ const         wpDataCalculator1 = await {
             let scaledMinutes1 = (minutes1 * 100) / 60;
             // let otTime = parseFloat(`${hours1}.${scaledMinutes1}`).toFixed(2) || 0;
             // let otTime = ((parseFloat(hours1 || 0) *60) + parseFloat(scaledMinutes1 || 0 ) /60).toFixed(2) || 0;
-            let otTime = ((parseFloat(hours1 || 0) *60) + parseFloat(scaledMinutes1 || 0 ) % 60)
-            workOfOT = 100;
+            let otTime = ((parseFloat(hours1 || 0) *60) + parseFloat(scaledMinutes1 || 0 ) % 600)
             
             // tmp.otTimes = `${hours1}.${scaledMinutes1}` || 0;
 
