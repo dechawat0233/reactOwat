308b5c4ae0a25dc13fdab4a84903ca51afbe7749
WasanPangpounju <friendlydev.net@gmail.com>
2024-08-26 10:59:02 +0700

    update

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -982,7 +982,7 @@ data.specialDayListWork = await intersection || [];
 }
 
 //check emty data 
-if(data.calSP  > 0) {
+if(data.accountingRecord.amountSpecialDay > 0) {
 const salaryRecord = new accounting(data);
 await salaryRecord.save();
 // await console.log(salaryRecord);
