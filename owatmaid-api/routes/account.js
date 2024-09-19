8633a760a76c820f70f9373347105b4ab4c14e03
WasanPangpounju <friendlydev.net@gmail.com>
2024-09-19 16:32:32 +0700

    update

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -725,7 +725,7 @@ if(tmp.id === item.id) {
   
   if(parseFloat(responseConclude.data.recordConclude[c].concludeRecord[i].workRate) > 0) {
     if(parseFloat(item.SpSalary) >= 363) {
-      // tmp.SpSalary = parseFloat(tmp.SpSalary) + (parseFloat(item.SpSalary)/ 30);
+      tmp.SpSalary = parseFloat(tmp.SpSalary) + (parseFloat(item.SpSalary)/ 30);
 
     } else {
       tmp.SpSalary = parseFloat(tmp.SpSalary) + parseFloat(item.SpSalary);
@@ -1858,7 +1858,7 @@ if(! dayW.includes( getDayNumberFromDate( responseConclude.data.recordConclude[c
       addSalaryDayArray.map(tmp => {
     if(tmp.id === item.id) {
       checkAddSalaryDay   = true;
-      tmp.SpSalary = parseFloat(tmp.SpSalary) + parseFloat(item.SpSalary);
+      // tmp.SpSalary = parseFloat(tmp.SpSalary) + parseFloat(item.SpSalary);
       tmp.message = parseFloat(tmp.message || 1) + 1;
     
     }
