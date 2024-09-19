a9f1ae282c45dd2b1be81ac40d933a1004fcb3fd
WasanPangpounju <friendlydev.net@gmail.com>
2024-09-19 18:42:31 +0700

    update

--- owatmaid-api/routes/account.js
+++ owatmaid-api/routes/account.js
@@ -727,9 +727,10 @@ if(tmp.id === item.id) {
   if(parseFloat(responseConclude.data.recordConclude[c].concludeRecord[i].workRate) > 0) {
     if(parseFloat(item.SpSalary) >= 363) {
       if((tmp.message || 0) > 0) {
-        tmp.SpSalary = (parseFloat(tmp.SpSalary) + (parseFloat(item.SpSalary)/ 30)).toFixed(2);
+        // tmp.SpSalary = (parseFloat(tmp.SpSalary) + (parseFloat(item.SpSalary)/ 30)).toFixed(2);
+        tmp.SpSalary = tmp.SpSalary  +1;
       } else {
-        // tmp.SpSalary = (parseFloat(item.SpSalary)/ 30).toFixed(2);
+        tmp.SpSalary = (parseFloat(item.SpSalary)/ 30).toFixed(2);
         tmp.SpSalary = 0;
 
       }
