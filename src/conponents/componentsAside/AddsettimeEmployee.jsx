dc0cd9f5a4b9510b417986277ae2b046b7d7658a
WasanPangpounju <friendlydev.net@gmail.com>
2024-11-26 12:31:17 +0700

    edit timerecord method push

--- src/conponents/componentsAside/AddsettimeEmployee.jsx
+++ src/conponents/componentsAside/AddsettimeEmployee.jsx
@@ -1886,8 +1886,6 @@ function AddsettimeEmployee() {
       month: month,
       employee_workplaceRecord: rowDataList2,
     };
-alert(timeRecord_id)
-return;
     try {
       const response = await axios.put(
         endpoint + "/timerecord/updateemp/" + timeRecord_id,
