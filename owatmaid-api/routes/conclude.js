90a34225971c2e5a8b7d5ae4ef678a7fa3422fea
WasanPangpounju <friendlydev.net@gmail.com>
2024-12-06 07:10:00 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -624,7 +624,10 @@ const         wpDataCalculator1 = await {
 
                 if (otTime >= workOfOT) {
                   otTime = workOfOT;
-                  tmp.otTimes = (((workOfOT_subHour * 60)+ workOfOT_subMinute ) - workOfOT_breakMinute) / 60;
+                  // tmp.otTimes = (((workOfOT_subHour * 60)+ workOfOT_subMinute ) - workOfOT_breakMinute) / 60;
+                  tmp.otTimes = (workOfOT_subHour + workOfOT_subMinute) + '-' + workOfOT_breakMinute;
+                  (((workOfOT_subHour * 60)+ workOfOT_subMinute ) - workOfOT_breakMinute) / 60;
+
                 } else {
                   tmp.otTimes = otTime || 0;
                 }
