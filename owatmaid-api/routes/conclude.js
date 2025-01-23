dc85e652034ed7eb5576b7bc1db61c277b0a7a6b
WasanPangpounju <friendlydev.net@gmail.com>
2025-01-24 01:13:11 +0700

    update

--- owatmaid-api/routes/conclude.js
+++ owatmaid-api/routes/conclude.js
@@ -873,7 +873,7 @@ const         wpDataCalculator1 = await {
                   tmp.allTimes = allTime || 0;
                 }
 
-                let workRate = ((parseFloat(tmpWP.data.holiday) * (salary / 8)) * parseFloat(allTime));
+                // let workRate = ((parseFloat(tmpWP.data.holiday) * (salary / 8)) * parseFloat(allTime));
                 // console.log('test' + tmpWP.data.holidayHour + ' ' + specialDayOff);
 console.log(json.stringify(tmpWP.data) )
                 tmp.workRate = workRate || 0;
